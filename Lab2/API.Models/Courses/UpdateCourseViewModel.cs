9315ab983b4182e2be27cf61c31950c941f739a3
Steinar Árnason <steinara13@ru.is>
2015-09-06 17:13:06 +0000

    viewmodel added

---
+++ Lab2/API.Models/Courses/UpdateCourseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.Courses
+{
+
+	/// <summary>
+	/// Updates the start and update of the course
+	/// </summary>
+	public class UpdateCourseViewModel
+	{
+		[Required]
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+	}
+}
