51b4485d91aaf51297f75efe32d2831beb48d82f
kristinn93 <kristinnj13@ru.is>
2015-09-08 13:49:49 +0000

    xml

--- Lab2/Lab2/Areas/HelpPage/App_Start/HelpPageConfig.cs
+++ Lab2/Lab2/Areas/HelpPage/App_Start/HelpPageConfig.cs
@@ -12,6 +12,8 @@ using System.Net.Http.Headers;
 using System.Reflection;
 using System.Web;
 using System.Web.Http;
+using API.Areas.HelpPage;
+
 #if Handle_PageResultOfT
 using System.Web.Http.OData;
 #endif
@@ -34,7 +36,7 @@ namespace Lab2.Areas.HelpPage
         public static void Register(HttpConfiguration config)
         {
             //// Uncomment the following to use the documentation from XML documentation file.
-            config.SetDocumentationProvider(new XmlDocumentationProvider(HttpContext.Current.Server.MapPath("~/App_Data/XmlDocument.xml")));
+            config.SetDocumentationProvider(new XmlDocumentationProvider(HttpContext.Current.Server.MapPath("~/App_Data/XmlDocument.xml"), HttpContext.Current.Server.MapPath("~/App_Data/apimodels.xml")));
 
             //// Uncomment the following to use "sample string" as the sample for all actions that have string as the body parameter or return type.
             //// Also, the string arrays will be used for IEnumerable<string>. The sample objects will be serialized into different media type 
